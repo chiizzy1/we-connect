195a16cfd46113d76ca11bafe0e73eff408e3ce8
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-27 15:12:49 +0100

    added user authentication

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
+const authController = require("../controllers/auth");
+const postsController = require("../controllers/posts")
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+// Login
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+// Sign up
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
+// Profile page
+router.get("/profile", ensureAuth, postsController.getProfile);
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,20 +3,26 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require('connect-mongo');
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const mainRoutes = require("./routes/main")
-
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+// const postRoutes = require("./routes/posts");
+// const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
+// Passport config
+require("./config/passport")(passport);
+
+//Connect To Database
+connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
-
 
 //Static Folder
 app.use(express.static("public"));
@@ -28,20 +34,20 @@ app.use(express.json());
 //Logging
 app.use(logger("dev"));
 
-
 //Use forms for put / delete
 app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB
 app.use(
-    session({
-      secret: "keyboard cat",
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.DB_STRING
     })
+  })
 );
-
 
 // Passport middleware
 app.use(passport.initialize());
@@ -50,15 +56,12 @@ app.use(passport.session());
 //Use flash messages for errors, info, ect...
 app.use(flash());
 
-
-// Set Up listening routes
+//Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-
-
-
-
+// app.use("/post", postRoutes);
+// app.use("/comment", commentRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-    console.log("Server is running, you better catch it!");
+  console.log("Server is running, you better catch it!");
 });
